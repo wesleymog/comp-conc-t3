88edec7c589e5b8cb2d4083c9bc0f5aa45b44c28
Wesley Mota <weselleymotabr@gmail.com>
2021-03-03 03:03:40 -0300

    +Versao 2 com tudo funcionando, sequencial e paralelo

--- trab3.c
+++ trab3.c
@@ -2,36 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "timer.h"
-
+#define DEBUG 1
 #define NTHREADS  3
 
-/* Variaveis globais */
+/*Estruturas*/
 struct thread_2_params{
-  long long int pos_inicial_atual;
+  // Acompanha a posição da primeira incidencia do valor que estamos analisando
+  long long int pos_inicial_atual;  
+  // Guarda a posição inicial do que tem a maior incidência dentre os analisados
   long long int pos_inicial_final;
+  // Guarda o numero de repetições do valor atual
   long long int tam_atual;
+  // Guarda o numero de repetições do valor de maior incidência
   long long int tam_final;
+  // Guarda o valor atual que está sendo analisado
   int valor_atual;
+  // Guarda o valor da maior repetição
   int valor_final;
 };
-struct thread_2_params tparams2;
+
 struct thread_3_params{
   long long int n_3repet;
   int n_repet;
   int valor_atual;
 };
-struct thread_3_params tparams3;
+
 struct thread_4_params{
   long long int n_05repet;
   int valor_atual;
 };
-struct thread_4_params tparams4;
+
+/* Definição de funções */
+struct thread_2_params verifica_2();
+struct thread_3_params verifica_3();
+struct thread_4_params verifica_0_5();
+
+/* Variaveis globais */
+struct thread_2_params tparams2, tp2;
+struct thread_3_params tparams3, tp3;
+struct thread_4_params tparams4, tp4;
 
 int x = 0;
 pthread_mutex_t x_mutex;
 pthread_cond_t x_cond;
 long long int M, *buffer_init, *buffer, *buffer_check;
 int g_t1=0, g_t2=0, g_t3=0, g_t4=0, N;
+char *filename;
 
 
 /* Thread 2 */
@@ -44,28 +60,13 @@ void *thread2 (void *t) {
       pthread_cond_wait(&x_cond, &x_mutex);
       //printf("Thread 2: sinal recebido e mutex realocado, x = %d\n", x);
     }
+    pthread_mutex_unlock(&x_mutex);
     local = g_t2;
     long long int final = g_t2+N;
     if(final> M)
       final=M;
     g_t2=final;
-    pthread_mutex_unlock(&x_mutex);
-    for(local; local<final; local++){
-        int valor = buffer[local];
-        if(tparams2.valor_atual == valor){
-          tparams2.tam_atual++;
-        }
-        if(tparams2.tam_atual>tparams2.tam_final){
-          tparams2.tam_final=tparams2.tam_atual;
-          tparams2.pos_inicial_final = tparams2.pos_inicial_atual;
-          tparams2.valor_final = tparams2.valor_atual;
-        }
-        if(tparams2.valor_atual != valor){
-          tparams2.tam_atual = 1;
-          tparams2.pos_inicial_atual = local;
-          tparams2.valor_atual = valor;
-        }
-    }
+    tparams2 = verifica_2(buffer, tparams2, local, final);
   }
   pthread_cond_broadcast(&x_cond);
   pthread_exit(NULL);
@@ -82,28 +83,16 @@ void *thread3 (void *t) {
       pthread_cond_wait(&x_cond, &x_mutex);
       //printf("Thread 3: sinal recebido e mutex realocado, x = %d\n", x);
     }
+    pthread_mutex_unlock(&x_mutex); 
+    
     local = g_t3;
-
     long long int final= g_t3+N;
     if(g_t3+N > M){
       final=M;
     }
     g_t3=final;
 
-    pthread_mutex_unlock(&x_mutex); 
-    for(local; local<final; local++){
-        int valor = buffer[local];
-        if(valor != tparams3.valor_atual){
-              tparams3.valor_atual = valor;
-              tparams3.n_repet = 0;
-            }
-        tparams3.n_repet++;
-        if(tparams3.n_repet == 3){
-            tparams3.n_repet = 0;
-            tparams3.valor_atual = -1;
-            tparams3.n_3repet++;            
-        }
-    }
+    tparams3 = verifica_3(buffer, tparams3, local, final);
   }
   //printf("até mais thread 3!\n");
   pthread_cond_broadcast(&x_cond);
@@ -128,18 +117,7 @@ void *thread4 (void *t) {
     }
     g_t4=final;
     pthread_mutex_unlock(&x_mutex); 
-    for(local; local<final; local++){
-        int valor = buffer[local];
-        if(valor == tparams4.valor_atual+1){
-              tparams4.valor_atual++;
-        }else{
-          tparams4.valor_atual=-1;
-        }
-        if(tparams4.valor_atual == 5){
-          tparams4.valor_atual = -1;
-          tparams4.n_05repet++;
-        }
-    }
+    tparams4 = verifica_0_5(buffer, tparams4, local, final);
   }
   //printf("até mais 4!\n");
   pthread_cond_broadcast(&x_cond);
@@ -147,18 +125,35 @@ void *thread4 (void *t) {
 }
 
 void write(long long int *nums){
- FILE *fptr = fopen("number.bin", "wb");
+ FILE *fptr = fopen(filename, "wb");
  fwrite(nums,sizeof(long long int),M,fptr);
  fclose(fptr);
 }
 
 void read(long long int *nums2){
- FILE *fptr = fopen("number.bin", "rb");
+ FILE *fptr = fopen(filename, "rb");
   for(long long int j=0;j<M;j++){
     fread(&nums2[j],sizeof(long long int),1,fptr);
     if(j%N == 0){
       g_t1 = j;
       pthread_cond_broadcast(&x_cond);
+    }
+  }
+  g_t1=M;
+ fclose(fptr);
+}
+
+void read_recurrence(long long int *nums2){
+ FILE *fptr = fopen(filename, "rb");
+ int local;
+  for(long long int j=0;j<M;j++){
+    fread(&nums2[j],sizeof(long long int),1,fptr);
+    if(j%N == 0){
+      local = g_t1;
+      g_t1 = j;
+      tp2 = verifica_2(nums2, tp2, local, j);
+      tp3 = verifica_3(nums2, tp3, local, j);
+      tp4 = verifica_0_5(nums2, tp4, local, j);
     }
   }
   g_t1=M;
@@ -188,68 +183,62 @@ struct thread_4_params initT4( struct thread_4_params tparams){
   return tparams;
 }
 
-void verifica_2(long long int *nums){
-  long long int j=0;
-  struct thread_2_params tp2;
-  tp2 = initT2(tp2);
-  while(j<M){
-    if(tp2.valor_atual == nums[j]){
-      tp2.tam_atual++;
+struct thread_2_params verifica_2(long long int *nums, struct thread_2_params tparams, 
+  long long int j, long long int max){
+  while(j<max){
+    if(tparams.valor_atual == nums[j]){
+      tparams.tam_atual++;
     }
-    if(tp2.tam_atual>tp2.tam_final){
-      tp2.tam_final=tp2.tam_atual;
-      tp2.pos_inicial_final = tp2.pos_inicial_atual;
-      tp2.valor_final = tp2.valor_atual;
+    if(tparams.tam_atual>tparams.tam_final){
+      tparams.tam_final=tparams.tam_atual;
+      tparams.pos_inicial_final = tparams.pos_inicial_atual;
+      tparams.valor_final = tparams.valor_atual;
     }
-    if(tp2.valor_atual != nums[j]){
-      tp2.tam_atual = 1;
-      tp2.pos_inicial_atual = j;
-      tp2.valor_atual = nums[j];
+    if(tparams.valor_atual != nums[j]){
+      tparams.tam_atual = 1;
+      tparams.pos_inicial_atual = j;
+      tparams.valor_atual = nums[j];
     }
     j++;
   }
-  long long int k=tp2.pos_inicial_final;
+  return tparams;
 }
 
-long long int verifica_3(long long int *nums){
-  long long int j=0;
-  long long int verifica3=0;
-
-  long long int valor=-1;
-  long long int repeat=0;
-  while(j<M){
-    if(buffer[j] != valor){
-          valor = nums[j];
-          repeat = 0;
+struct thread_3_params verifica_3(long long int *nums, struct thread_3_params tparams, 
+  long long int j, long long int max){
+  while(j<max){
+    if(nums[j] != tparams.valor_atual){
+          tparams.valor_atual = nums[j];
+          tparams.n_repet = 0;
         }
-    repeat++;
-    if(repeat == 3){
-        repeat = 0;
-        valor = -1;
-        verifica3++;
+    tparams.n_repet++;
+    if(tparams.n_repet == 3){
+        tparams.n_repet = 0;
+        tparams.valor_atual = -1;
+        tparams.n_3repet++;
     }
     j++;
   }
-  return verifica3;
+  return tparams;
 }
 
-long long int verifica_0_5(long long int *nums){
-  long long int j=0;
-  long long int verifica0_5=0;
+struct thread_4_params verifica_0_5(long long int *nums, struct thread_4_params tparams, 
+  long long int j, long long int max
+  ){
   long long int valor=-1;
-  while(j<M){
-    if(nums[j] == valor+1){
-          valor++;
+  while(j<max){
+    if(nums[j] == tparams.valor_atual+1){
+      tparams.valor_atual++;
     }else{
-      valor=-1;
+      tparams.valor_atual=-1;
     }
-    if(valor == 5){
-      valor = -1;
-      verifica0_5++;
+    if(tparams.valor_atual == 5){
+      tparams.valor_atual = -1;
+      tparams.n_05repet++;
     }
     j++;
   }
-  return verifica0_5;
+  return tparams;
 }
 /* Funcao principal */
 int main(int argc, char *argv[]) {
@@ -260,6 +249,7 @@ int main(int argc, char *argv[]) {
   // inicialização das variáveis
   N = atoi(argv[1]);
   M = atoll(argv[2]);
+  filename = "number1.bin";
   buffer_init = (long long int *) malloc(sizeof( long long int) * M);
   buffer = (long long int *) malloc(sizeof( long long int) * M);
   buffer_check = (long long int *) malloc(sizeof( long long int) * M);
@@ -269,6 +259,16 @@ int main(int argc, char *argv[]) {
     printf("--ERRO: malloc()\n"); exit(-1);}
   if(buffer_check==NULL) {
     printf("--ERRO: malloc()\n"); exit(-1);}
+
+  tparams2 = initT2(tparams2);
+  tparams3 = initT3(tparams3);
+  tparams4 = initT4(tparams4);
+  tp2 = initT2(tp2);
+  tp3 = initT3(tp3);
+  tp4 = initT4(tp4);
+
+  //identificadores das threads
+  pthread_t threads[NTHREADS];
   long long int i=0;
   while(i < M){
     buffer_init[i] = rand()%6;   
@@ -276,11 +276,6 @@ int main(int argc, char *argv[]) {
   }
   write(buffer_init);
   
-  tparams2 = initT2(tparams2);
-  tparams3 = initT3(tparams3);
-  tparams4 = initT4(tparams4);
-  //identificadores das threads
-  pthread_t threads[NTHREADS];
 
   /* Inicilaiza o mutex (lock de exclusao mutua) e a variavel de condicao */
   pthread_mutex_init(&x_mutex, NULL);
@@ -313,21 +308,24 @@ int main(int argc, char *argv[]) {
   }
   //Printando resultados
   printf(
-    "Maior sequência de valores idênticos: %lld %lld %d \nQuantidade de triplas: %lld \nQuantidade de ocorrências da sequência <012345>: %lld", 
+    "Maior sequência de valores idênticos: %lld %lld %d \nQuantidade de triplas: %lld \nQuantidade de ocorrências da sequência <012345>: %lld\n", 
     tparams2.pos_inicial_final, tparams2.tam_final, 
     tparams2.valor_final, tparams3.n_3repet, tparams4.n_05repet
     );
   
   GET_TIME(inicio);
-  read(buffer_check);
-  verifica_2(buffer_check);
-  verifica_0_5(buffer_check);
-  verifica_3(buffer_check);
-
+  read_recurrence(buffer_check);
   GET_TIME(fim);
   tempo_continua = fim-inicio;
+  /*
   printf(
-    "\n%lf %lf %lf\n", 
+    "Maior sequência de valores idênticos: %lld %lld %d \nQuantidade de triplas: %lld \nQuantidade de ocorrências da sequência <012345>: %lld\n", 
+    tp2.pos_inicial_final, tp2.tam_final, 
+    tp2.valor_final, tp3.n_3repet, tp4.n_05repet
+    );
+    */
+  printf(
+    "\nSequencial - Paralela - Aceleração\n%lf  -- %lf  -- %lf\n", 
     tempo_continua, tempo_paralel,tempo_continua/tempo_paralel
     );
   /* Desaloca variaveis e termina */
